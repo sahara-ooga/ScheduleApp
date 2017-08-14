d608c389f6ff0dd1846a7eaef0bf765b62d35793
yuu ogasawara <y.ogasawara@st-ventures.jp>
2017-08-14 11:13:22 +0900

    CocoapodsでFMDBを導入

---
+++ Pods/Target Support Files/Pods-ScheduleApp/Pods-ScheduleApp-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ScheduleAppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ScheduleAppVersionString[];
+
---
+++ Pods/Target Support Files/Pods-ScheduleAppUITests/Pods-ScheduleAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ScheduleAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ScheduleAppUITests
+@end
