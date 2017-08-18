a610cee2de8ad132baf25a2feea4029fd4da27f8
sahara-ooga <y.ogasawara@st-ventures.jp>
2017-08-18 11:16:51 +0900

    MonthViewProviderを追加
    テストファイルも追加
    セルを呼び出すためにreuseIdentifierを追加

---
+++ ScheduleApp/Views/MonthView/MonthViewProvider.swift
@@ -0,0 +1,43 @@
+//
+//  MonthViewProvider.swift
+//  ScheduleApp
+//
+//  Created by Yuu Ogasa on 2017/08/18.
+//  Copyright © 2017年 Sunday Carpenter. All rights reserved.
+//
+
+import UIKit
+
+class MonthViewProvider: NSObject {
+    
+}
+
+//MARK: UICollectionViewDataSource
+extension MonthViewProvider:UICollectionViewDataSource{
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return MonthViewSettings.kNumberOfSection
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: MonthViewCell.self),
+                                                      for: indexPath) as! MonthViewCell
+        cell.set(for:indexPath)
+        return cell
+    }
+}
+
+//MARK: Constants
+struct MonthViewSettings{
+    static let kNumberOfSection = 2
+    
+    enum SectionName:Int {
+        case yobi = 0,
+        day
+    }
+}
