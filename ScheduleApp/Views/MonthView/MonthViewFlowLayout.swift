fb368aa6cc92eff40cfd954b661793ee65eeb144
yuu ogasawara <y.ogasawara@st-ventures.jp>
2017-08-21 22:24:09 +0900

    FlowLayout,MonthVCの導入

---
+++ ScheduleApp/Views/MonthView/MonthViewFlowLayout.swift
@@ -0,0 +1,41 @@
+//
+//  MonthViewFlowLayout.swift
+//  ScheduleApp
+//
+//  Created by yogasawara@stv on 2017/08/21.
+//  Copyright © 2017年 Sunday Carpenter. All rights reserved.
+//
+
+import UIKit
+
+class MonthViewFlowLayout: NSObject,UICollectionViewDelegateFlowLayout {
+    let cellMargin:CGFloat = 2.0
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let daysParWeek:CGFloat = CGFloat(MonthViewSettings.kNumberOfItemInYobiSection)
+        let numberOfMargin:CGFloat =  CGFloat(daysParWeek + 1)
+        let width:CGFloat = (collectionView.frame.size.width - cellMargin * numberOfMargin) / daysParWeek
+        let height:CGFloat = width * 1.5
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: cellMargin, left: cellMargin,
+                            bottom: cellMargin, right: cellMargin)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return cellMargin
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return cellMargin
+    }
+}
