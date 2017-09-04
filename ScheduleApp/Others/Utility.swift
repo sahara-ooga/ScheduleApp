fe31a01d75a51c8794d2fe6cf554f36fd879a5ca
sahara-ooga <y.ogasawara@st-ventures.jp>
2017-09-04 13:17:31 +0900

    アラートメッセージを表示するメソッドを追加

--- ScheduleApp/Others/Utility.swift
+++ ScheduleApp/Others/Utility.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2017年 Sunday Carpenter. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct Utility {
     static func numberOfDays(in month:Month, of year:Int)->Int?{
@@ -27,4 +27,79 @@ struct Utility {
         return Int(dateString)
     }
 
+    // *****************
+    // MARK: Alert Message
+    // *****************
+    /// OK アラートメッセージダイアログを表示する
+    ///
+    /// - Parameters:
+    ///   - title: タイトル
+    ///   - message: メッセージ
+    ///   - vc: 表示元VC & delegate
+    ///   - nextSelector: 閉じた後実行するセレクタ
+    static func showOkAlertMessage(title: String?, message: String?,
+                                  vc: UIViewController,
+                                  nextSelector: Selector?) {
+        
+        let alertController = UIAlertController(title: title,
+                                                message: message,
+                                                preferredStyle: .alert)
+        
+        let alertAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            //OK
+            let delegate = vc as? AlertMessageDelegate
+            delegate?.completeActionBoAlertMessage(action: action,
+                                                   nextSelector: nextSelector)
+        }
+        
+        alertController.addAction(alertAction)
+        
+        // 表示
+        vc.present(alertController, animated: true, completion: nil)
+    }
+    
+    /// OK キャンセル 確認メッセージダイアログを表示する
+    ///
+    /// - Parameters:
+    ///   - title: タイトル
+    ///   - message: メッセージ
+    ///   - vc: 表示元VC & delegate
+    ///   - nextSelector: ダイアログを閉じた後実行するセレクタ ※OK・キャンセルどちらを押しても実行される
+    static func showOkConfirmMessage(title: String, message: String?,
+                                    vc: UIViewController,
+                                    nextSelector: Selector?) {
+        let alertController = UIAlertController(title: title,
+                                                message: message, preferredStyle: .alert)
+        
+        let delegate = vc as? AlertMessageDelegate
+        
+        let okAction = UIAlertAction(title: "OK",
+                                     style: .default) { (action) in
+            //OK
+            delegate?.completeActionBoAlertMessage(action: action,
+                                                   nextSelector: nextSelector)
+        }
+        alertController.addAction(okAction)
+        
+        let cancelAction = UIAlertAction(title: "キャンセル",
+                                         style: .cancel) { (action) in
+            //Cancel
+            delegate?.completeActionBoAlertMessage(action: action,
+                                                   nextSelector: nextSelector)
+        }
+        
+        alertController.addAction(cancelAction)
+        
+        //表示
+        vc.present(alertController, animated: true, completion: nil)
+    }
+}
+
+/**
+ *  プロトコル
+ */
+protocol AlertMessageDelegate : class {
+    func completeActionBoAlertMessage(action: UIAlertAction,
+                                      nextSelector: Selector?)
+    
 }
