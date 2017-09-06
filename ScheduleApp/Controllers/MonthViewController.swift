b98d5ed04f79433a3b5c6eb62079e06f66f13a6b
yuu ogasawara <y.ogasawara@st-ventures.jp>
2017-09-06 23:24:27 +0900

    CollectionViewの設定をStoryboardとコードに追加
    CollectionViewが表示されるようになった（バグを含む）

--- ScheduleApp/Controllers/MonthViewController.swift
+++ ScheduleApp/Controllers/MonthViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MonthViewController: UIViewController {
 
+    var monthViewDataSource:UICollectionViewDataSource!
+    var flowLayout:UICollectionViewDelegateFlowLayout!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setCollectionView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
 
+extension MonthViewController{
+    func setCollectionView() {
+        collectionView.register(UINib.init(nibName: String(describing: MonthViewCell.self),
+                                           bundle: nil),
+                                forCellWithReuseIdentifier: String(describing: MonthViewCell.self))
+        let monthViewProvider = MonthViewProvider()
+        monthViewProvider.selectedYear = Date().year
+        monthViewProvider.selectedMonth = Date().month
+        monthViewDataSource = monthViewProvider
+        flowLayout = MonthViewFlowLayout()
+        
+        collectionView.dataSource = monthViewDataSource
+        collectionView.delegate = flowLayout
+    }
 }
 
