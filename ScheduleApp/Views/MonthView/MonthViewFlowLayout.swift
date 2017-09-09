4f1ca39a2a4caf33925caa25ec2228b0ce36573e
yuu ogasawara <y.ogasawara@st-ventures.jp>
2017-09-09 09:27:45 +0900

    曜日と日にちのセルの高さを分ける
    曜日と日のセルの高さを分けて区別がつくようにした

--- ScheduleApp/Views/MonthView/MonthViewFlowLayout.swift
+++ ScheduleApp/Views/MonthView/MonthViewFlowLayout.swift
@@ -13,11 +13,28 @@ class MonthViewFlowLayout: NSObject,UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let daysParWeek:CGFloat = CGFloat(MonthViewSettings.kNumberOfItemInYobiSection)
-        let numberOfMargin:CGFloat =  CGFloat(daysParWeek + 1)
-        let width:CGFloat = (collectionView.frame.size.width - cellMargin * numberOfMargin) / daysParWeek
-        let height:CGFloat = width * 1.5
-        return CGSize(width: width, height: height)
+        switch indexPath.section {
+        case MonthViewSettings.Section.yobi.rawValue:
+            let daysParWeek:CGFloat = CGFloat(MonthViewSettings.kNumberOfItemInYobiSection)
+            let numberOfMargin:CGFloat =  CGFloat(daysParWeek + 1)
+            let width:CGFloat = (collectionView.frame.size.width - cellMargin * numberOfMargin) / daysParWeek
+            let height:CGFloat = width * 0.3
+            return CGSize(width: width, height: height)
+            
+        case MonthViewSettings.Section.day.rawValue:
+            let daysParWeek:CGFloat = CGFloat(MonthViewSettings.kNumberOfItemInYobiSection)
+            let numberOfMargin:CGFloat =  CGFloat(daysParWeek + 1)
+            let width:CGFloat = (collectionView.frame.size.width - cellMargin * numberOfMargin) / daysParWeek
+            let height:CGFloat = width * 1.5
+            return CGSize(width: width, height: height)
+            
+        default:
+            let daysParWeek:CGFloat = CGFloat(MonthViewSettings.kNumberOfItemInYobiSection)
+            let numberOfMargin:CGFloat =  CGFloat(daysParWeek + 1)
+            let width:CGFloat = (collectionView.frame.size.width - cellMargin * numberOfMargin) / daysParWeek
+            let height:CGFloat = width * 1.5
+            return CGSize(width: width, height: height)
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView,
