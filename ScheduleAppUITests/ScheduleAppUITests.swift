d2f29844b9dbf09ab3331d65bf3ba10023259dd6
yuu ogasawara <y.ogasawara@st-ventures.jp>
2017-09-23 23:58:01 +0900

    DayViewCellのテストクラスを追加して、グループを作成

--- ScheduleAppUITests/ScheduleAppUITests.swift
+++ ScheduleAppUITests/ScheduleAppUITests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import ScheduleApp
 
 class ScheduleAppUITests: XCTestCase {
         
@@ -26,11 +27,6 @@ class ScheduleAppUITests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
-    }
-    
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
 }
